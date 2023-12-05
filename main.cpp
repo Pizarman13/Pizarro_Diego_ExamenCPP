02aa726d6f3b36d1bcbde4330a76336e776da587
Pizarman13 <146822082+Pizarman13@users.noreply.github.com>
2023-12-05 10:08:48 +0000

    ej3 metodo lookup

--- main.cpp
+++ main.cpp
@@ -13,8 +13,11 @@ public:
         symbolTable.insert(std::pair<std::string, int>(symbol, value));
     }
 
-    int getSymbol(const std::string& name) {
-        return symbolTable[name];
+    int lookup(const std::string& name) {
+    if (symbolTable.find(name) == symbolTable.end()) {
+        throw std::runtime_error("No existe el simbolo");
+    }
+    return symbolTable[name];
     }
 };
 
@@ -28,8 +31,14 @@ int main()
     env.insert("x", 10);
     env.insert("y", 20);
 
-    cout << "x es " << env.getSymbol("x") << endl;
-    cout << "y es " << env.getSymbol("y") << endl;
+    cout << "x es " << env.lookup("x") << endl;
+    cout << "y es " << env.lookup("y") << endl;
+    
+    try {
+        cout << "z es " << env.lookup("z") << endl;  // Debería lanzar una excepción
+    } catch (const std::runtime_error& e) {
+        cout << e.what() << endl;  // Debería imprimir "No existe el simbolo"
+    }
 
     return 0;
 }
