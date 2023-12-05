98546b830b7b8de3fefd1f5d7111fffd88c49a80
Pizarman13 <146822082+Pizarman13@users.noreply.github.com>
2023-12-05 09:58:40 +0000

    Ej1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <map>
+#include <string>
 
 using namespace std;
+
+class Environment {
+private:
+    std::map<std::string, int> symbolTable;
+
+public:
+    Environment() {}
+
+    void set(std::string var, int val) {
+        symbolTable[var] = val;
+    }
+
+    int get(std::string var) {
+        return symbolTable[var];
+    }
+};
+
 
 int main()
 {
     cout << "Hello World!" << endl;
+
+    Environment env;
+
+    env.set("x", 1);
+    env.set("y", 2);
+
+    cout << env.get("x") << endl;
+    cout << env.get("y") << endl;
+
     return 0;
 }
