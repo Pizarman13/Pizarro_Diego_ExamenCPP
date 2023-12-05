553e9d309133c58caccd7039289afc4fa3794426
Pizarman13 <146822082+Pizarman13@users.noreply.github.com>
2023-12-05 10:44:32 +0000

    implementacion del variant

--- main.cpp
+++ main.cpp
@@ -2,26 +2,28 @@
 #include <map>
 #include <string>
 #include <stdexcept>
+#include <variant>
 
 using namespace std;
 
 class Environment {
 private:
-    std::map<std::string, int> symbolTable;
+    std::map<std::string, std::variant<int, double, std::string>> symbolTable;
 
 public:
-    void insert(const std::string& name, int value) {
+    template <typename T>
+    void insert(const std::string& name, T value) {
         if (symbolTable.find(name) != symbolTable.end()) {
-            throw std::runtime_error("No existe el simbolo");
+            throw std::runtime_error("Symbol already exists");
         }
         symbolTable[name] = value;
     }
 
-    int lookup(const std::string& name) {
-    if (symbolTable.find(name) == symbolTable.end()) {
-        throw std::runtime_error("No existe el simbolo");
-    }
-    return symbolTable[name];
+    std::variant<int, double, std::string> lookup(const std::string& name) {
+        if (symbolTable.find(name) == symbolTable.end()) {
+            throw std::runtime_error("Symbol does not exist");
+        }
+        return symbolTable[name];
     }
 };
 
@@ -33,15 +35,18 @@ int main()
     Environment env;
 
     env.insert("x", 10);
-    env.insert("y", 20);
+    env.insert("y", 20.5);
+    env.insert("z", "Hola");
 
-    cout << "x es " << env.lookup("x") << endl;
-    cout << "y es " << env.lookup("y") << endl;
+    std::cout << "x es " << std::get<int>(env.lookup("x")) << std::endl;
+    std::cout << "y es " << std::get<double>(env.lookup("y")) << std::endl;
+    std::cout << "z es " << std::get<std::string>(env.lookup("z")) << std::endl;
+
     
     try {
-        cout << "z es " << env.lookup("z") << endl;  // Debería lanzar una excepción
+        std::cout << "w es " << std::get<int>(env.lookup("w")) << std::endl;  // Debería lanzar una excepción
     } catch (const std::runtime_error& e) {
-        cout << e.what() << endl;  // Debería imprimir "No existe el simbolo"
+        std::cout << e.what() << std::endl;  // Debería imprimir "Symbol does not exist"
     }
 
     return 0;
